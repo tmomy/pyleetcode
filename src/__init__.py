f4c6c3c1a56b770cdb33d63b44821bcac62e6cca
tmomy <datmomy@gmail.com>
2018-02-26 17:58:13 +0800

    Initial commit

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@author: Tmomy
+@time: 2018/2/26 17:42
+"""
---
+++ src/ordered_dict.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@author: Tmomy
+@time: 2018/2/26 17:42
+"""
+from collections import OrderedDict
+
+class SortedDict(dict):
+    pass
